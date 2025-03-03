9d5a91f9e501184cf526c0443fbc13968a26116e
DmitryTheCreator <dmitriyzamorev@gmail.com>
2025-03-03 19:16:05 +0500

    new: Кастомный компонент инпута

---
+++ src/shared/ui/input/base/BaseInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <input
+    class="ui-input"
+    :type="type"
+    :value="modelValue"
+    @input="updateValue"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits, InputTypeHTMLAttribute } from "vue";
+
+interface IProps {
+  modelValue?: string | number;
+  type?: InputTypeHTMLAttribute;
+}
+
+const { modelValue = "", type = "text" } = defineProps<IProps>();
+const emit = defineEmits<{
+  "update:modelValue": [modelValue: string];
+}>();
+
+const updateValue = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  emit("update:modelValue", input.value);
+};
+</script>
+
+<style lang="scss">
+@use "styles";
+</style>
---
+++ src/shared/ui/input/index.ts
@@ -0,0 +1,3 @@
+import BaseInput from "./base/BaseInput.vue";
+
+export { BaseInput };

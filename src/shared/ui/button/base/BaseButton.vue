764590df80d48bcaaba6b9cab3a3d2125d39ae6e
DmitryTheCreator <dmitriyzamorev@gmail.com>
2025-03-11 19:24:45 +0500

    new: Кастомный компонент кнопки

---
+++ src/shared/ui/button/base/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button type="button" class="ui-button" :class="variantClass">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+interface Props {
+  variant?: "primary" | "secondary" | "success" | "danger" | "warning" | "dark";
+}
+
+const props = defineProps<Props>();
+
+const variantClass = computed(() => `ui-button--${props.variant || "primary"}`);
+</script>
+
+<style scoped lang="scss">
+@use "styles";
+</style>

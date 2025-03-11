237ce0081807fba80da2480943bdda6d45c40932
DmitryTheCreator <dmitriyzamorev@gmail.com>
2025-03-11 19:18:43 +0500

    chg: Хранилище на уровне провайдеров

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { store } from "./providers";
 
-export const app = createApp(App);
+export const app = createApp(App).use(store);
